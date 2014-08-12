641b70ea6c62648dfa10d8c4df37b2870663c3aa
Shin Ishimaru <shinishimaru@gmail.com>
2014-08-12 16:43:26 +0900

    スクリーンキャプチャ使ってみたので、それ用のスクリプトを追加

---
+++ Assets/ScreenCapture.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var folder = "ScreenshotFolder";
+var frameRate = 25;
+
+function Start () {
+
+    // Set the playback framerate (real time will not relate to game time after this).
+    Time.captureFramerate = frameRate;
+ 
+    // Create the folder
+    System.IO.Directory.CreateDirectory(folder);
+    
+}
+
+function Update () {
+
+    // Append filename to folder name (format is '0005 shot.png"')
+    var name = String.Format("{0}/{1:D04} shot.png", folder, Time.frameCount );
+ 
+    // Capture the screenshot to the specified file.
+    Application.CaptureScreenshot(name);
+}
